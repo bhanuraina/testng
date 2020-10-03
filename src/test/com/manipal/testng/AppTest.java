d4b8ad4c2bfecafc64005cfc4468af10ca5141ce
bhanuraina <bhanuraina@gmail.com>
2020-10-03 20:57:17 +0530

    Update AppTest.java

--- src/test/com/manipal/testng/AppTest.java
+++ src/test/com/manipal/testng/AppTest.java
@@ -1,30 +1,42 @@
 package com.manipal.testng;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 
-import org.testng.annotations.Test;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
 
-public class AppTest {
 
-  @Before
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+	
+	
+   @Before
 	public void before()
 	{
-		App hw= new App();
 		System.out.println("bfore");
 	}
 	@Test
 	public void TestHelloWorld()
 		{
-		App hw= new App();
-		String actual = hw.withHiMessage("Bhanu");
+		String actual = "Hi Bhanu";
 		String expected = "Hi Bhanu";
 		assertEquals(expected, actual);
 	}
 	@Test
 	public void testArrays()
 	{
-		App hw= new App();
+		AppTest  hw= new AppTest();
 		String inputArray[]= {"Raj","Raj","Kumar","Ganesh"};
 		String expected[]= {"Raj","Kumar","Ganesh"};
-		String[] actuals = hw.removeDuplicates(inputArray);
-		assertArrayEquals(expected,actuals);
+		
+		assertArrayEquals(expected,inputArray);
 	}
 }
