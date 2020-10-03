75387d0350622965ca4f2aecfbe909c46c90c591
bhanuraina <bhanuraina@gmail.com>
2020-10-03 20:29:35 +0530

    Update AppTest.java

--- src/test/java/com/manipal/testng/AppTest.java
+++ src/test/java/com/manipal/testng/AppTest.java
@@ -12,10 +12,27 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-     System.out.println("Hello");
-        assertTrue( true );
-    }
+   @Before
+	public void before()
+	{
+		HelloWorld hw= new HelloWorld();
+		System.out.println("bfore");
+	}
+	@Test
+	public void TestHelloWorld()
+		{
+		HelloWorld hw= new HelloWorld();
+		String actual = hw.withHiMessage("Bhanu");
+		String expected = "Hi Bhanu";
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void testArrays()
+	{
+		HelloWorld hw= new HelloWorld();
+		String inputArray[]= {"Raj","Raj","Kumar","Ganesh"};
+		String expected[]= {"Raj","Kumar","Ganesh"};
+		String[] actuals = hw.removeDuplicates(inputArray);
+		assertArrayEquals(expected,actuals);
+	}
 }
