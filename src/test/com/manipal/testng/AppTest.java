838b08e042c6ad51f015b5e3fdf7f3ce9f954f4d
bhanuraina <bhanuraina@gmail.com>
2020-10-03 21:33:43 +0530

    Update AppTest.java

--- src/test/com/manipal/testng/AppTest.java
+++ src/test/com/manipal/testng/AppTest.java
@@ -21,7 +21,27 @@ public class AppTest
    @Before
 	public void before()
 	{
-		System.out.println("bfore");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Acer\\Downloads\\chromedriver_win32\\chromedriver.exe");
+WebDriver dri = new ChromeDriver();
+dri.manage().window().maximize();
+dri.get("https://opensource-demo.orangehrmlive.com/");
+dri.manage().window().maximize();
+dri.findElement(By.name("txtUsername")).click();
+dri.findElement(By.name("txtUsername")).clear();
+System.out.println(dri.getTitle());
+dri.findElement(By.xpath("//*[@id='txtUsername']")).sendKeys("Admin");
+dri.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys("admin123");
+
+System.out.println(dri.findElement(By.xpath("//*[@id='txtPassword']")).getAttribute("id"));
+dri.findElement(By.xpath("//*[@id='btnLogin']")).click();
+
+
+System.out.println(dri.findElement(By.xpath("//*[@id='welcome-menu']")).getAttribute("id"));
+dri.findElement(By.xpath("//*[@id='welcome']")).click();
+Thread.sleep(200);
+dri.findElement(By.xpath("//*[@id='welcome-menu']/ul/li[2]/a")).click();
+Thread.sleep(200);
+dri.close();
 	}
 	@Test
 	public void TestHelloWorld()
