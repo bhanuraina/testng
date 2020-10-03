468a977f602cc3b66e114b18463ff0049c79b644
bhanu <you@example.com>
2020-10-03 12:33:05 +0530

    adding project

---
+++ src/main/java/com/manipal/testng/App.java
@@ -0,0 +1,30 @@
+package com.manipal.testng;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Driver;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+class App {
+WebDriver driver;
+	@Test
+		public void testcase1() throws MalformedURLException
+	{
+		DesiredCapabilities cap =  DesiredCapabilities.chrome();
+		cap.setBrowserName("chrome");
+		cap.setPlatform(Platform.ANY);
+		driver = new RemoteWebDriver(new URL("http://192.168.1.85:4444/wd/hub"), cap);
+		driver.manage().window().maximize();
+		driver.navigate().to("http://www.google.com");
+		System.out.println(driver.getTitle());
+		
+	}
+}
---
+++ src/test/com/manipal/testng/AppTest.java
@@ -0,0 +1,11 @@
+package com.manipal.testng;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+  @Test
+  public void testcase1Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
